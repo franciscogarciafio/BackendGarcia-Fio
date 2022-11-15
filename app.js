f3462788f7d7b7c644cca293130d4b292fd02003
Francisco Garcia Fio <fran_810@hotmail.com>
2022-11-14 23:59:48 -0300

    primer trabajo practico

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-console.log("hola mundo");
-console.log ("hola cabezon")
+class Usuario{
+    constructor(nombre,apellido,libros,mascotas){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.libros=[libros];
+        this.mascotas=[mascotas];
+    }
+
+
+        getFullName() {
+            return `${this.nombre}` + " " + `${this.apellido}`;
+             
+        
+        }
+
+        
+        addMascota() {
+            return (this.mascotas + " " + "elefante")
+        }
+    
+        CountMascotas() {
+            return (this.mascotas.length);
+        }
+        getbooknames() {
+            return (this.libros)
+        }
+
+}
+
+const Usuario1 = new Usuario("francisco","Garcia","harry Potter,narnia","perro,gato")
+const usuario2= new Usuario("maria","lopez","pricipito,metamorfosis","gato,perro,raton")
+//console.log(Usuario1);
+
+console.log(Usuario1.getFullName());
+console.log(Usuario1.addMascota());
+console.log(Usuario1.CountMascotas());
+console.log(Usuario1.getbooknames());
